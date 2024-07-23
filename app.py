f6953d763d53c47da0ccfa92bbd7450e14448ce5
Harvey <Jak3grason@gmail.com>
2024-07-23 15:16:31 -0400

    add test for game

--- app.py
+++ app.py
@@ -1,14 +1,14 @@
 from flask import Flask
 import probability
 import pygame
-
+import random
+import numpy as np
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello_world():  # put application's code here
     return 'Hello World!'
-
 
 if __name__ == '__main__':
     app.run()
