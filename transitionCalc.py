28079365e2630787e30e6c2f0886fc3f2bba3b0f
Harvey <Jak3grason@gmail.com>
2024-07-25 16:10:21 -0400

    Update transitionCalc.py

--- transitionCalc.py
+++ transitionCalc.py
@@ -9,7 +9,8 @@ for dice1 in die:
     for dice2 in die:
         for dice3 in die:
             for dice4 in die:
-                combinations.append([dice1, dice2, dice3, dice4])
+                for dice5 in die:
+                    combinations.append([dice1, dice2, dice3, dice4, dice5])
 
 # Initialize counters
 team_A_wins = 0
@@ -19,32 +20,53 @@ ties = 0
 # Iterate through each combination
 for combination in combinations:
     # Split into teams
-    dice1, dice2, dice3, dice4 = combination
-    # Compare dice1 with dice3 and dice2 with dice4
+    dice1, dice2, dice3, dice4, dice5 = combination
 
-    [6,6,6,6]
     greatest_defense = 0
     greatest_attack = 0
+
     second_greatest_defense = 0
     second_greatest_attack = 0
-    if(dice1>=dice2):
+
+    third_greatest_attack = 0
+
+    # defense dice check
+    if (dice1 >= dice2):
         greatest_defense = dice1
         second_greatest_defense = dice2
     else:
         greatest_defense = dice2
         second_greatest_defense = dice1
-    if(dice3>=dice4):
-        greatest_attack = dice3
-        second_greatest_attack = dice4
-    else:
-        greatest_attack = dice4
-        second_greatest_attack = dice3
 
-    if((greatest_attack > greatest_defense) and (second_greatest_defense >= second_greatest_attack) or (greatest_defense>=greatest_attack) and (second_greatest_attack>second_greatest_defense)):
+    # attack dice check
+    if (dice3 >= dice4 and dice3 >= dice5):
+        greatest_attack = dice3
+        if (dice4 >= dice5):
+            second_greatest_attack = dice4
+            third_greatest_attack = dice5
+        else:
+            second_greatest_attack = dice5
+            third_greatest_attack = dice4
+    elif (dice4 >= dice5 and dice4 >= dice3):
+        greatest_attack = dice4
+        if (dice5 >= dice3):
+            second_greatest_attack = dice5
+            third_greatest_attack = dice3
+    else:
+        greatest_attack = dice5
+        if (dice3 >= dice4):
+            second_greatest_attack = dice3
+            third_greatest_attack = dice4
+        else:
+            second_greatest_attack = dice4
+            third_greatest_attack = dice3
+
+    if ((greatest_attack > greatest_defense) and (second_greatest_defense >= second_greatest_attack) or (
+            greatest_defense >= greatest_attack) and (second_greatest_attack > second_greatest_defense)):
         ties += 1
-    elif((greatest_defense >= greatest_attack) and (second_greatest_defense >= second_greatest_attack)):
+    elif ((greatest_defense >= greatest_attack) and (second_greatest_defense >= second_greatest_attack)):
         team_D_wins += 1
-    elif((greatest_defense < greatest_attack) and (second_greatest_defense < second_greatest_attack)):
+    elif ((greatest_defense < greatest_attack) and (second_greatest_defense < second_greatest_attack)):
         team_A_wins += 1
 
 # Print the results
