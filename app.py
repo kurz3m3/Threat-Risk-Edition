062ccff35fabc0e705cb1d1e98cbe47cedee0a2f
Harvey <Jak3grason@gmail.com>
2024-08-01 13:39:53 -0400

    test

--- app.py
+++ app.py
@@ -1,14 +1,11 @@
-from flask import Flask
-import probability
-import pygame
-import random
-import numpy as np
+# app.py
+from flask import Flask, render_template
+
 app = Flask(__name__)
 
-
 @app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+def home():
+    return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
